1feac22207b5489c9c63ac677ca8ed6730bbed7f
Cristian Estupiñan <93301195+Cristianguerrer@users.noreply.github.com>
2025-06-11 15:44:42 -0500

    update

--- n8n-aws-docker-k8s/modules/ec2/elb.tf
+++ n8n-aws-docker-k8s/modules/ec2/elb.tf
@@ -22,6 +22,8 @@ resource "aws_lb_target_group" "n8n" {
 }
 
 resource "aws_lb_listener" "https" {
+  depends_on = [aws_acm_certificate_validation.ssl] # ← Esto fuerza el orden
+
   load_balancer_arn = aws_lb.n8n.arn
   port              = 443
   protocol          = "HTTPS"
@@ -34,6 +36,21 @@ resource "aws_lb_listener" "https" {
 
   tags = merge(var.tags, { Name = "${var.instance_name}-listener" })
 }
+
+
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.n8n.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate.ssl.arn
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.n8n.arn
+#   }
+
+#   tags = merge(var.tags, { Name = "${var.instance_name}-listener" })
+# }
 
 resource "aws_lb_target_group_attachment" "n8n" {
   target_group_arn = aws_lb_target_group.n8n.arn
