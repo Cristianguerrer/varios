7b405a3dd09a09fcbc7aee51f93a2f68750ad78a
Cristian Estupiñan <93301195+Cristianguerrer@users.noreply.github.com>
2025-06-10 23:35:06 -0500

    Add Route53 zone and EIP for n8n instance

--- n8n-aws-docker-k8s/envs/dev/main.tf
+++ n8n-aws-docker-k8s/envs/dev/main.tf
@@ -6,3 +6,15 @@ module "n8n_instance" {
   vpc_cidr      = var.vpc_cidr
   subnet_cidr   = var.subnet_cidr
 }
+
+resource "aws_route53_zone" "public" {
+  name = var.domain
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.public.zone_id
+  name    = var.domain
+  type    = "A"
+  ttl     = 300
+  records = [module.n8n_instance.public_ip]
+}
