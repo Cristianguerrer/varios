831e77f4ae59a58c0f1d3db62d31577ae286618d
Cristian Estupiñan <93301195+Cristianguerrer@users.noreply.github.com>
2025-06-11 11:40:27 -0500

    Add domain variable and outputs for Route53

--- n8n-aws-docker-k8s/modules/ec2/outputs.tf
+++ n8n-aws-docker-k8s/modules/ec2/outputs.tf
@@ -17,3 +17,11 @@ output "subnet_id" {
 output "lb_dns_name" {
   value = aws_lb.n8n.dns_name
 }
+
+output "route53_zone_id" {
+  value = aws_route53_zone.public.zone_id
+}
+
+output "route53_record_name" {
+  value = aws_route53_record.root.fqdn
+}
