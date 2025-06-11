24c6d2d997ab62f0da83f5ff207588c026e63ff1
Cristian Estupiñan <93301195+Cristianguerrer@users.noreply.github.com>
2025-06-11 14:33:20 -0500

    add alias route53 alb

--- n8n-aws-docker-k8s/modules/ec2/route53.tf
+++ n8n-aws-docker-k8s/modules/ec2/route53.tf
@@ -6,6 +6,18 @@ resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.public.zone_id
   name    = var.domain
   type    = "A"
-  ttl     = 300
-  records = [aws_eip.n8n.public_ip]
+
+  alias {
+    name                   = aws_lb.n8n.dns_name
+    zone_id                = aws_lb.n8n.zone_id
+    evaluate_target_health = true
+  }
 }
+
+# resource "aws_route53_record" "root" {
+#   zone_id = aws_route53_zone.public.zone_id
+#   name    = var.domain
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_eip.n8n.public_ip]
+# }
