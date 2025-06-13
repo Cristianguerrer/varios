4c6ca56af968517b9516cbc92c61d266584f49e2
Cristian Estupiñan <93301195+Cristianguerrer@users.noreply.github.com>
2025-06-13 08:29:00 -0500

    update

--- n8n-aws/docker-tf-pro/modules/dns_acm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "hosted_zone_id" {
-  value = aws_route53_zone.public.zone_id
-}
-
-output "certificate_arn" {
-  value = aws_acm_certificate.ssl.arn
-}
--- n8n-aws/docker-tf-pro/modules/dns_acm/zone.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_route53_zone" "public" {
-  name = var.domain
-}
